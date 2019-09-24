92937a84a97b9cee3fd5d5f5a368c05b433acaac
Pontus Svedman <pontus.svedman@novacura.se>
2019-09-24 21:41:34 +0200

    Added firebase firestore

--- app/src/main/java/com/appzet/taghunt/MainActivity.kt
+++ app/src/main/java/com/appzet/taghunt/MainActivity.kt
@@ -2,11 +2,45 @@ package com.appzet.taghunt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        private val TAG = "MainActivity"
+    }
+
+    val db = FirebaseFirestore.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var button = findViewById<Button>(R.id.button)
+        val editText = findViewById<EditText>(R.id.editText)
+
+
+        button.setOnClickListener{
+
+            var _first = editText.text.toString()
+            addToFirebase(_first, "mylastname")
+        }
+    }
+
+    fun addToFirebase(first: String, second: String) {
+        var user = hashMapOf(
+            "first" to first,
+            "second" to second
+        )
+        db.collection("users")
+            .add(user)
+            .addOnSuccessListener { documentReference ->
+                Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                Log.w(TAG, "Error adding document", e)
+            }
     }
 }
