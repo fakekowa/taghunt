b2cc89515e8d3d7963bd7f922c156fcf80dd3770
Aleksander <aleksander@baam.se>
2019-09-25 19:19:17 +0200

    Skapat Lobby aktivitet, rooms aktivitet(med recycle view)

---
+++ app/src/main/java/com/appzet/taghunt/create_user_activity.kt
@@ -0,0 +1,12 @@
+package com.appzet.taghunt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class create_user_activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_user_activity)
+    }
+}
---
+++ app/src/main/java/com/appzet/taghunt/lobby_activity.kt
@@ -0,0 +1,12 @@
+package com.appzet.taghunt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class lobby_activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lobby_activity)
+    }
+}
