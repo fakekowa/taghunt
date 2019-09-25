970e697ca924d3bd9f3d167c8e9e4530c4fdd67f
Aleksander <aleksander@baam.se>
2019-09-25 21:52:11 +0200

    Added Create new game activity, and join game dialog

---
+++ app/src/main/java/com/appzet/taghunt/activities/RoomsActivity.kt
@@ -0,0 +1,14 @@
+package com.appzet.taghunt.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.appzet.taghunt.R
+
+class RoomsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.rooms_activity)
+    }
+
+}
