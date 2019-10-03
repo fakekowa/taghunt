30c0bd2e6950002604b24adf9dcf7e1179a22559
pontus svedman <svedmanp@gmail.com>
2019-10-03 23:30:34 +0200

    Improved Create game activity design

--- app/src/main/java/com/appzet/taghunt/activities/CreateGameActivity.kt
+++ app/src/main/java/com/appzet/taghunt/activities/CreateGameActivity.kt
@@ -1,22 +1,35 @@
 package com.appzet.taghunt.activities
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import android.widget.Button
-import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
 import com.appzet.taghunt.R
-import kotlinx.android.synthetic.main.create_game_activity.*
+import com.google.android.material.textfield.TextInputEditText
 
 
 class CreateGameActivity: AppCompatActivity() {
+
+    var editTextFilledExposedDropdown: AutoCompleteTextView? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.create_game_activity)
+
+        val textInputEditText = findViewById<TextInputEditText>(R.id.createGameActivityTextInput)
+
+        editTextFilledExposedDropdown =
+            findViewById(R.id.createGamePlayerDropdown)
+
+        //Focus username input and show keyboard
+        textInputEditText.requestFocus()
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, InputMethodManager.HIDE_IMPLICIT_ONLY)
 
         val button1 = findViewById<Button>(R.id.create_game_activity_start_game_button)
         button1.setOnClickListener {
@@ -24,52 +37,41 @@ class CreateGameActivity: AppCompatActivity() {
             startActivity(intent)
         }
 
-        // Create an ArrayAdapter for number of players
-        val adapter = ArrayAdapter.createFromResource(this,
-            R.array.create_game_activity_array_list, android.R.layout.simple_spinner_item)
-        // Specify the layout to use when the list of choices appears
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        // Apply the adapter to the spinner
-        //create_game_activity_spinner.adapter = adapter
-
-        //Attempt to shorten the spinner dropdown list
-        //val spinner = findViewById<Spinner>(R.id.create_game_activity_spinner)
-        //try {
-        //    val popup = Spinner::class.java.getDeclaredField("mPopup")
-        //    popup.isAccessible = true
-        //    // Get private mPopup member variable and try cast to ListPopupWindow
-        //    val popupWindow = popup.get(spinner) as android.widget.ListPopupWindow
-        //    // Set popupWindow height to 700px
-        //    popupWindow.height = 500
-        //}
-        //catch(e: Exception){
-//
-        //}
-
-        CreatePlayableAreaAdapter(adapter)
-
-        CreateGameTimerAdapter(adapter)
+        //Populate the "Spinners"
+        CreatePlayerAdapter()
+        CreatePlayableAreaAdapter()
+        CreatePlayTimeAdapter()
     }
 
-    private fun CreatePlayableAreaAdapter(adapter: ArrayAdapter<CharSequence>) {
+    private fun CreatePlayerAdapter() {
         // Create an ArrayAdapter for playable area
-        val adapter2 = ArrayAdapter.createFromResource(
+        val adapter = ArrayAdapter.createFromResource(
+            this,
+            R.array.create_game_activity_No_Players_list, android.R.layout.simple_spinner_item
+        )
+        editTextFilledExposedDropdown =
+            findViewById<AutoCompleteTextView>(R.id.createGamePlayerDropdown)
+        editTextFilledExposedDropdown!!.setAdapter(adapter)
+    }
+    private fun CreatePlayableAreaAdapter() {
+        // Create an ArrayAdapter for playable area
+        val adapter1 = ArrayAdapter.createFromResource(
             this,
             R.array.create_game_activity_playablearea_list, android.R.layout.simple_spinner_item
         )
-        var editTextFilledExposedDropdown = findViewById<AutoCompleteTextView>(R.id.filled_exposed_dropdown)
+        var editTextFilledExposedDropdown =
+            findViewById<AutoCompleteTextView>(R.id.createGameActivityPlayAreaDropdown)
+        editTextFilledExposedDropdown.setAdapter(adapter1)
+    }
+    private fun CreatePlayTimeAdapter() {
+        // Create an ArrayAdapter for playable area
+        val adapter2 = ArrayAdapter.createFromResource(
+            this,
+            R.array.create_game_activity_play_time_list, android.R.layout.simple_spinner_item
+        )
+        var editTextFilledExposedDropdown =
+            findViewById<AutoCompleteTextView>(R.id.createGameActivityPlayTimeDropdown)
         editTextFilledExposedDropdown.setAdapter(adapter2)
     }
 
-    private fun CreateGameTimerAdapter(adapter: ArrayAdapter<CharSequence>) {
-        // Create an ArrayAdapter for play time
-        val adapter3 = ArrayAdapter.createFromResource(
-            this,
-            R.array.create_game_activity_array_list3, android.R.layout.simple_spinner_item
-        )
-        // Specify the layout to use when the list of choices appears
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        // Apply the adapter to the spinner
-        create_game_activity_spinner3.adapter = adapter3
-    }
 }
