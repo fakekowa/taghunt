3151858153b843a0202c8b9b6008d8690c68b749
LordSouzin <aleksander@baam.se>
2019-10-09 18:58:44 +0200

    Small changes to the Join game dialog TODO: Keyboard opens on startup, LobbyActivity dropdown focused on startup

--- app/src/main/java/com/appzet/taghunt/activities/RoomsActivity.kt
+++ app/src/main/java/com/appzet/taghunt/activities/RoomsActivity.kt
@@ -1,31 +1,29 @@
 package com.appzet.taghunt.activities
 
 import android.app.Dialog
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Window
 import android.widget.Button
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.appzet.taghunt.R
 
 class RoomsActivity : AppCompatActivity() {
-
-    val dialog = Dialog(this)
-    val joinBtn = dialog.findViewById(R.id.joinGameDialogButton) as Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.rooms_activity)
 
-        showDialog("thisismytitle")
+        showDialog()
     }
 
-    private fun showDialog(title: String) {
+    private fun showDialog() {
+        val dialog = Dialog(this)
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
         dialog.setCancelable(false)
         dialog.setContentView(R.layout.join_game_dialog)
         dialog.show()
 
+        val joinBtn = dialog.findViewById(R.id.joinGameDialogButton) as Button
         joinBtn.setOnClickListener {
             dialog.dismiss()
         }
